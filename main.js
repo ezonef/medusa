ea26451e751725331b4c4f7fde4fd82e2518d26e
ezonef <50654320+ezonef@users.noreply.github.com>
2022-04-10 19:47:51 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event){
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
-        
+
     });
 });
+
+const anchors = document.querySelectorAll(`a[href*="#"]`)
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute(`href`)
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        })
+    })
+}
